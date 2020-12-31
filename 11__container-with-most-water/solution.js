570886760d1f2923253768a4831d30d1d78c953d
Abdus <abdus@abdus.net>
2020-12-31 11:00:30 +0530

    solved

---
+++ 11__container-with-most-water/solution.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+const maxArea = function (height) {
+  let left = 0;
+  let right = height.length - 1;
+  let area = 0;
+
+  while (left < right) {
+    area = Math.max(
+      area,
+      Math.min(height[right], height[left]) * (right - left)
+    );
+
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return area;
+};
+
+maxArea([1, 8]);
