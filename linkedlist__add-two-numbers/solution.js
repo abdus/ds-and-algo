b40ce1db0e508b1a880ff6c192c190aedeac166a
Abdus <abdus@abdus.net>
2021-01-02 00:07:06 +0530

    updated on Sat Jan  2 12:07:06 AM +0530 2021

---
+++ linkedlist__add-two-numbers/solution.js
@@ -0,0 +1,16 @@
+//Definition for singly-linked list.
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+const addTwoNumbers = function (l1, l2) {};
+
+console.log(addTwoNumbers([2, 4, 3], [5, 6, 4]));
+console.log(addTwoNumbers([0], [0]));
+console.log(addTwoNumbers([9, 9, 9, 9, 9, 9, 9], [9, 9, 9, 9]));
