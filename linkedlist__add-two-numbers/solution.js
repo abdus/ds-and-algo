37ba0d64dc93476bbc8d9550518fee7c27d64290
Abdus <abdus@abdus.net>
2021-01-02 12:13:37 +0530

    updated on Sat Jan  2 12:13:37 PM +0530 2021

--- linkedlist__add-two-numbers/solution.js
+++ linkedlist__add-two-numbers/solution.js
@@ -9,7 +9,29 @@ function ListNode(val, next) {
  * @param {ListNode} l2
  * @return {ListNode}
  */
-const addTwoNumbers = function (l1, l2) {};
+const addTwoNumbers = function (l1, l2) {
+  const big = l1.length > l2.length ? l1 : l2;
+  const result = new ListNode(0);
+  let current = result;
+  let carry = 0;
+
+  let i = 0;
+  while (i < big.length || carry > 0) {
+    const x = l1[i] || 0;
+    const y = l2[i] || 0;
+
+    let sum = x + y + carry;
+    carry = Math.floor(sum / 10);
+    sum = sum % 10;
+
+    current.next = new ListNode(sum);
+    current = current.next;
+
+    i++;
+  }
+
+  return result.next;
+};
 
 console.log(addTwoNumbers([2, 4, 3], [5, 6, 4]));
 console.log(addTwoNumbers([0], [0]));
